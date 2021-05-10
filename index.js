6ac095bf94f3cf5627cb3304d202ef2db9d2f404
Jese310S <59402195+Jese310S@users.noreply.github.com>
2021-05-10 16:11:50 +1000

    add comments

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const inquirer = require("inquirer");
 const Table = require("cli-table");
 
-
+// api url for 25 tickets a page
 const url = 'https://jsole.zendesk.com/api/v2/tickets.json?page[size]=25';
 
 // gets all tickets from API
@@ -28,6 +28,7 @@ function getTickets(page) {
 // gets a single specified ticket from the API
 function getSingleTicket(id) {
     return new Promise((resolve, reject) => {
+        //api request
         axios.get(`https://jsole.zendesk.com/api/v2/tickets/${id}.json`, {
             auth: {
                 username: process.env.AUTH_USER,
@@ -40,9 +41,9 @@ function getSingleTicket(id) {
     });
 }
 
-//  prompts user to see next and previous pages 
-
+//pagination function that prompts user to see next and previous pages 
 const pagination = (prev, next) => {
+    //prompt user for next or prev page
     inquirer.prompt([{
         type: 'list',
         message: 'What would you like to do?', 
@@ -57,17 +58,18 @@ const pagination = (prev, next) => {
         if (answers.options === "Prev Page") {
             getTickets(prev).then((results, err) => {
                 if (err) console.log(err.code, "\nSomething went wrong, please check and try again.")
-
+                //instantiate table 
                 let table = new Table ({
                     head: ['ID', 'Created_at', 'Submitter_Id', 'Subject'], 
                     colWidths: [20, 20, 20, 40]
                 });
-
+                ////loop and push each tickets desired info to table array
                 results.tickets.forEach(ticket => {
                     table.push([ticket.id, ticket.created_at, ticket.submitter_id, ticket.subject])
                 });
-
+                //console table to be created by cli-table 
                 console.log(table.toString());
+                //makes sure prev does not appear when on first or last page
                 if (results.tickets[0].id > 25) {
                     pagination(results.links.prev, results.links.next);
                 } else {
@@ -77,20 +79,23 @@ const pagination = (prev, next) => {
             });
         } else if (answers.options === "Next Page") {
             getTickets(next).then((results, err) => {
+                //handle err
                 if (err) console.log(err.code, "\nSomething went wrong, please check and try again.")
-
+                //instantiate table
                 let table = new Table ({
                     head: ['ID', 'Created_at', 'Submitter_Id', 'Subject'], 
                     colWidths: [20, 20, 20, 40]
                 });
-
+                //loop and push each tickets desired info to cli-table array
                 results.tickets.forEach(ticket => {
                     table.push([ticket.id, ticket.created_at, ticket.submitter_id, ticket.subject])
                 });
-
+                //console.log table to be created by cli-table 
                 console.log(table.toString());
+                //makes sure prev does not appear when on first or last page
                 if (results.tickets[0].id > 25) {
-                    pagination(results.links.prev, results.links.next);
+                    
+                    pagination(results.links.prev, results.links.next);// api links to paginate and go to next and prev pages
                 } else {
                     pagination(false, results.links.next);
                 }
@@ -106,6 +111,7 @@ const pagination = (prev, next) => {
 // Main inquiry function for tickets
 
 const inquire = () => {
+    //prompt user with options 
     inquirer.prompt([{
         type: 'list', 
         message: 'What would you like to do?', 
@@ -118,27 +124,29 @@ const inquire = () => {
     }]).then(answers => {
         if (answers.options === "Display all tickets"){
             getTickets().then((results, err) => {
+                //handle err
                 if (err) console.log(err.code, "\nSomething went wrong, please check and try again.")
-
+                //instantiate table
                 let table = new Table ({
                     head: ['ID', 'Created_at', 'Submitter_Id', 'Subject'], 
                     colWidths: [20, 20, 20, 40]
                 });
-
+                //loop through tickets and push to the cli-table array
                 results.tickets.forEach(ticket => {
                     table.push([ticket.id, ticket.created_at, ticket.submitter_id, ticket.subject])
                 });
-
+                //console.log to make table
                 console.log(table.toString());
-
+                //makes sure prev option does not appear on first page
                 if (results.tickets[0].id > 25) {
-                    pagination(results.links.prev, results.links.next);
+                    
+                    pagination(results.links.prev, results.links.next);// api links to paginate and go to next and prev pages
                 } else {
                     pagination(false, results.links.next);
                 }
             });
         } else if (answers.options === "Display a ticket") {
-              // Prompt user to enter ticketID
+              // Prompt user to ask if they know ticket id number
               inquirer.prompt([
                   {
                   type:'list',
@@ -150,22 +158,24 @@ const inquire = () => {
                   ]}
               ]).then(answers => {
                   if (answers.options === 'yes'){
+                      //prompt user for ticket id number
                       inquirer.prompt([
                           {type: 'number', 
                           message: 'Enter ticket ID number',
                           name: 'idNum',
                         }
                       ]).then(ticketId => {
-                          //handle err
                           getSingleTicket(ticketId.idNum).then((ticket, err) => {
+                        //handle err
                               if(err) console.log(err.code, '\nPlease enter a valid ticket number');
-                                                    //instantiate Table
+                        //instantiate Table
                             var table = new Table(
                                 {
                                 head: ['Ticket']
                              , colWidths: [20, 60]
                                 }
                            );
+                           //Push the desired ticket info to the cli-table array
                             table.push(
                                 {'ID':ticket.id}, 
                                 {'Submitter Id': ticket.submitter_id},
@@ -173,6 +183,7 @@ const inquire = () => {
                                 {'URL': ticket.url}, 
                                 {'Description' : ticket.description}
                             );
+                            //console.log to show table
                             console.log(table.toString());
                              inquire();
                         })
@@ -181,6 +192,7 @@ const inquire = () => {
                   }
               })
         } else {
+            //print when user quits program
             console.log("Have a great day!")
         }
     });
