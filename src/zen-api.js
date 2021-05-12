cb0c195a5661b13f640b90c96c8a9624a135e4ae
Jese310S <59402195+Jese310S@users.noreply.github.com>
2021-05-12 13:45:56 +1000

    make functions asyn to work properly with testing

--- src/zen-api.js
+++ src/zen-api.js
@@ -1,39 +1,30 @@
-const axios = require("axios");
+const axios = require('axios');
 
 // api url for 25 tickets a page
 const url = 'https://jsole.zendesk.com/api/v2/tickets.json?page[size]=25';
 
 // gets all tickets from API
-function getTickets(page) {
-
-    return new Promise((resolve, reject) => {
-        axios.get((page) ? page : url, {
-            auth: {
-        //use dotenv to make environment variable, see .env_sample for examples
-                username: process.env.AUTH_USER,
-                password: process.env.AUTH_PASS
-            }
-        })
-        .then ( res => {
-            resolve(res.data); 
-        })
-    });
+async function getTickets(page) {
+  return await axios.get(page ? page : url, {
+    auth: {
+      //use dotenv to make environment variable, see .env_sample for examples
+      username: process.env.AUTH_USER,
+      password: process.env.AUTH_PASS
+    }
+  });
 }
 
 // gets a single specified ticket from the API
-function getSingleTicket(id) {
-    return new Promise((resolve, reject) => {
-        //api request
-        axios.get(`https://jsole.zendesk.com/api/v2/tickets/${id}.json`, {
-            auth: {
-                username: process.env.AUTH_USER,
-                password: process.env.AUTH_PASS
-            }
-        })
-        .then ( res => {
-            resolve(res.data.ticket);  
-        })
-    });
+async function getSingleTicket(id) {
+  return await axios.get(
+    `https://jsole.zendesk.com/api/v2/tickets/${id}.json`,
+    {
+      auth: {
+        username: process.env.AUTH_USER,
+        password: process.env.AUTH_PASS
+      }
+    }
+  );
 }
 
 module.exports.getTickets = getTickets;
